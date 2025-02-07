302c5d81b3d97f9ca26cdb5a4ec6f99c45560d87
Zachary Morgan (zgf) <zjmorgan@mtu.edu>
2025-02-07 11:34:50 -0500

    Auto centroid

--- src/garnet/reduction/data.py
+++ src/garnet/reduction/data.py
@@ -524,13 +524,13 @@ class BaseDataModel:
 
         """
 
-        signal = mtd[num].getSignalArray().copy()
-        error_sq = mtd[num].getErrorSquaredArray().copy()
-        mask = signal <= 0
-        signal[mask] = np.nan
-        error_sq[mask] = np.nan
-        mtd[num].setSignalArray(signal)
-        mtd[num].setErrorSquaredArray(error_sq)
+        # signal = mtd[num].getSignalArray().copy()
+        # error_sq = mtd[num].getErrorSquaredArray().copy()
+        # mask = signal <= 0
+        # signal[mask] = np.nan
+        # error_sq[mask] = np.nan
+        # mtd[num].setSignalArray(signal)
+        # mtd[num].setErrorSquaredArray(error_sq)
 
         DivideMD(LHSWorkspace=num, RHSWorkspace=den, OutputWorkspace=ws)
 
